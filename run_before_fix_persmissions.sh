1fac7a95f93490cab599997c5b5e16587b60924a
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-02-28 16:27:54 +0100

    feat: add gedit

--- run_before_fix_persmissions.sh
+++ run_before_fix_persmissions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 chezmoi_dir="$(chezmoi source-path)"
-for d in AppData dot_cargo private_dot_config; do
+for d in AppData dot_cargo private_dot_config dot_local; do
     fdfind -t f '.*' "$chezmoi_dir/$d" -x chmod 644 '{}'
 done
