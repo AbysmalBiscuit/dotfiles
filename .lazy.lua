a5d2cc8952192c3586db97051e5456049e6cebbf
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-03-03 13:39:38 +0100

    style: run formatter on .lazy.lua

--- .lazy.lua
+++ .lazy.lua
@@ -1,25 +1,25 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function()
-			local orig_start = vim.treesitter.start
-			local orig_get_parser = vim.treesitter.get_parser
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local orig_start = vim.treesitter.start
+      local orig_get_parser = vim.treesitter.get_parser
 
-			vim.treesitter.get_parser = function(buf, lang, ...)
-				buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
-				if vim.b[buf].chezmoi_ts_lang then
-					lang = vim.b[buf].chezmoi_ts_lang
-				end
-				return orig_get_parser(buf, lang, ...)
-			end
+      vim.treesitter.get_parser = function(buf, lang, ...)
+        buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
+        if vim.b[buf].chezmoi_ts_lang then
+          lang = vim.b[buf].chezmoi_ts_lang
+        end
+        return orig_get_parser(buf, lang, ...)
+      end
 
-			vim.treesitter.start = function(buf, lang, ...)
-				buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
-				if vim.b[buf].chezmoi_ts_lang then
-					lang = vim.b[buf].chezmoi_ts_lang
-				end
-				return orig_start(buf, lang, ...)
-			end
-		end,
-	},
+      vim.treesitter.start = function(buf, lang, ...)
+        buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
+        if vim.b[buf].chezmoi_ts_lang then
+          lang = vim.b[buf].chezmoi_ts_lang
+        end
+        return orig_start(buf, lang, ...)
+      end
+    end,
+  },
 }
