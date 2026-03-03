0e469f293b6afa6362db877fbd377fb8b21efd46
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-03-03 12:56:57 +0100

    refactor: move scripts to .chezmoiscripts/ dir

--- .chezmoiscripts/run_before_fix_persmissions.sh
+++ .chezmoiscripts/run_before_fix_persmissions.sh
@@ -4,3 +4,5 @@ chezmoi_dir="$(chezmoi source-path)"
 for d in AppData dot_cargo private_dot_config dot_local; do
     fdfind -t f '.*' "$chezmoi_dir/$d" -x chmod 644 '{}'
 done
+
+fdfind -t d '.*' "$chezmoi_dir/dot_local" -x chmod 750 '{}'
