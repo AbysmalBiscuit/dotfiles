5871753c69ca42c86f9d7c87de69f2c8e3cff71b
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-03-10 14:11:48 +0100

    style: update env_compiler.sh to use '# {{' as the chezmoi left delimeter

--- .chezmoitemplates/sh/env_compiler.sh
+++ .chezmoitemplates/sh/env_compiler.sh
@@ -1,11 +1,12 @@
+# chezmoi:template:left-delimiter="# {{" right-delimiter="}}"
 # some common environment variables to optimise compilation
 # export commands that are commented out are the defaults in /etc/makepkg.conf
 # To see latest default makepkg.conf:
 # https://gitlab.archlinux.org/archlinux/packaging/packages/pacman/-/blob/main/makepkg.conf
 
 # system and architecture
-ARCH={{ .arch | quote }}
-CHOST={{ .chost | quote }}
+ARCH='# {{ .arch | quote }}'
+CHOST='# {{ .chost | quote }}'
 export ARCH
 export CHOST
 
@@ -38,55 +39,63 @@ export NPY_NUM_BUILD_JOBS="$(nproc)"
 # rust
 # -C link-arg=-z -C link-arg=pack-relative-relocs -C force-frame-pointers=yes
 export RUSTFLAGS='-C target-cpu=native'
-{{ if .is_windows -}}
-{{- /* Windows linker doesn't support the linker args */ -}}
+# {{ if .is_windows -}}
+# {{- /* Windows linker doesn't support the linker args */ -}}
 RUSTFLAGS_RELEASE="${RUSTFLAGS} -C opt-level=3 -C debuginfo=none -C debug_assertions=no -C codegen-units=1"
-{{- else -}}
+# {{- else -}}
 RUSTFLAGS_RELEASE="${RUSTFLAGS} -C opt-level=3 -C debuginfo=none -C debug_assertions=no -C codegen-units=1 -C link-arg=-z -C link-arg=pack-relative-relocs"
-{{- end }}
+# {{- end }}
 export RUSTFLAGS_RELEASE
 
-{{- if .is_macos }}
+# {{- if .is_macos }}
 # Setting compiler variables for specific libraries
-if [[ -d "/usr/local/opt/openblas" ]]; then
-  export LDFLAGS="$LDFLAGS -L/usr/local/opt/openblas/lib"
-  export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/openblas/include"
-fi
+# {{- if stat "/usr/local/opt/openblas" }}
+# if [[ -d "/usr/local/opt/openblas" ]]; then
+export LDFLAGS="$LDFLAGS -L/usr/local/opt/openblas/lib"
+export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/openblas/include"
+# fi
+# {{- end }}
 
-if [[ -d "/usr/local/opt/qt" ]]; then
-  export LDFLAGS="$LDFLAGS -L/usr/local/opt/qt/lib"
-  export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/qt/include"
-fi
+# {{- if stat "/usr/local/opt/qt" }}
+# if [[ -d "/usr/local/opt/qt" ]]; then
+export LDFLAGS="$LDFLAGS -L/usr/local/opt/qt/lib"
+export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/qt/include"
+# fi
+# {{- end }}
 
-if [[ -d "/usr/local/opt/llvm" ]]; then
-  export LDFLAGS="$LDFLAGS -L/usr/local/opt/llvm/lib"
-  export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/llvm/include"
-fi
-{{- end }}
+# {{- if stat "/usr/local/opt/llvm" }}
+# if [[ -d "/usr/local/opt/llvm" ]]; then
+export LDFLAGS="$LDFLAGS -L/usr/local/opt/llvm/lib"
+export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/llvm/include"
+# fi
+# {{- end }}
+# {{- end }}
 
 # go
-if has_command go; then
-    GOAMD64="v1"
-    temp_file=$(mktemp 'XXXXX.go')
-    printf 'package main\nfunc main() { println("testing go level") }\n' >"$temp_file"
+# {{- if .has.go }}
+# if has_command go; then
+GOAMD64="v1"
+temp_file=$(mktemp 'XXXXX.go')
+printf 'package main\nfunc main() { println("testing go level") }\n' >"$temp_file"
 
-    for level in 4 3 2 1; do
-        if GO111MODULE=off GOAMD64="v$level" go run "$temp_file" &>/dev/null; then
-            GOAMD64="v$level"
-            break
-        fi
-    done
-
-    # Final cleanup if the loop finishes without success
-    if [[ -f "$temp_file" ]]; then
-        rm "$temp_file"
+for level in 4 3 2 1; do
+    if GO111MODULE=off GOAMD64="v$level" go run "$temp_file" &>/dev/null; then
+        GOAMD64="v$level"
+        break
     fi
-    export GOAMD64
+done
+
+# Final cleanup if the loop finishes without success
+if [[ -f "$temp_file" ]]; then
+    rm "$temp_file"
 fi
+export GOAMD64
+# fi
+# {{- end }}
 
 # if [[ -f "$HOME/.cargo/bin/sccache" ]]; then
-	# export RUSTC_WRAPPER="$HOME/.cargo/bin/sccache"
-	# export SCCACHE_DIR="$HOME/.cache/sccache"
-	# export SCCACHE_DIRECT=true
-	# export SCCACHE_CONF="$HOME/.config/sccache/config.toml"
+# export RUSTC_WRAPPER="$HOME/.cargo/bin/sccache"
+# export SCCACHE_DIR="$HOME/.cache/sccache"
+# export SCCACHE_DIRECT=true
+# export SCCACHE_CONF="$HOME/.config/sccache/config.toml"
 # fi
