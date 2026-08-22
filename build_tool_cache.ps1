ad39915644c3103d973c023e711f33137bb41540
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-08-22 10:05:08 +0200

    fix(chezmoi,powershell): build tool cache script not updating chezmoi.toml

--- build_tool_cache.ps1
+++ build_tool_cache.ps1
@@ -1,3 +1,4 @@
 $SourceDir = "$env:USERPROFILE\.local\share\chezmoi"
 
 Get-Content "$SourceDir\.chezmoiscripts\windows\run_onchange_before_01-generate-has-cache.ps1.tmpl" -Raw | chezmoi execute-template | powershell -Command -
+chezmoi init
