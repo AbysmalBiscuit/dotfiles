de918fb7fb83ac87a49d5a1fe2f645f97b7efee1
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-08-26 15:24:19 +0200

    feat(fish): make right arrow complete path components instead of entire line

--- private_dot_config/fish/exact_functions/fish_user_key_bindings.fish
+++ private_dot_config/fish/exact_functions/fish_user_key_bindings.fish
@@ -7,4 +7,6 @@ function fish_user_key_bindings
     # resetting all bindings.
     # The argument specifies the initial mode (insert, "default" or visual).
     fish_vi_key_bindings --no-erase insert
+
+    bind --mode insert right forward-path-component
 end
