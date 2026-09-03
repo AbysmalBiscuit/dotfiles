57dc2cf6e0f4f38dce95ad0ab493a08b7317fd49
Lev Velykoivanenko <velykoivanenko.lev@gmail.com>
2026-09-03 20:09:11 +0200

    feat(neovim,tools): use custom patched taplo

--- private_dot_config/exact_nvim/after/lsp/taplo.lua
+++ private_dot_config/exact_nvim/after/lsp/taplo.lua
@@ -1,6 +1,8 @@
+local exe = vim.g.is_windows and "taplo.exe" or "taplo"
+
 ---@type vim.lsp.Config
 return {
-  cmd = { "taplo", "lsp", "stdio" },
+  cmd = { vim.fs.normalize("~/.cargo/bin/" .. exe), "lsp", "stdio" },
   filetypes = { "toml" },
   root_markers = { ".git", ".taplo.toml", "taplo.toml" },
 }
